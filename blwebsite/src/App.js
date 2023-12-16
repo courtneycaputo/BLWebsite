191c5fda2d5843a002c2a29b359f2588496876f1
Courtney Caputo <courtneycaputo@Courtneys-MacBook-Air.local>
2023-12-16 11:47:23 -0800

    update

--- blwebsite/src/App.js
+++ blwebsite/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Demos from './components/Demos';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="navbar">
+        <div className="logo"> </div>
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#demos">Demos</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
       </header>
+
+      <main>
+        <Home />
+        <About />
+        <Demos />
+        <Contact />
+      </main>
     </div>
   );
 }
